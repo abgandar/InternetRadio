4253276219ae19b366ba9352b07c02a06cc72abf
Alexander Wittig <alexander@wittig.name>
2017-12-31 10:52:27 +0100

    testing

--- src/ir-cgi.c
+++ src/ir-cgi.c
@@ -477,9 +477,10 @@ void loadMusic( const char *arg )
 {
     mpd_run_clear( conn );
     mpd_search_add_db_songs( conn, false );
-    mpd_search_add_uri_constraint( conn, MPD_OPERATOR_DEFAULT, arg ); // matches against full file name relative to music dir or uri. empty arg allowed.
+    //mpd_search_add_uri_constraint( conn, MPD_OPERATOR_DEFAULT, arg ); // matches against full file name relative to music dir or uri. empty arg allowed.
 //    if( arg && *arg != '\0' )
 //        mpd_search_add_base_constraint( conn, MPD_OPERATOR_DEFAULT, arg );  // restrict search to subdirectory of music dir, must be non-empty, error if directory does not exist
+    // none of these sort tags seem to work (error: "incorrect arguments")
     //mpd_search_add_sort_tag( conn, MPD_TAG_ARTIST_SORT, false );    // are multiple sort tags supported?
     //mpd_search_add_sort_tag( conn, MPD_TAG_ARTIST, false );
     //mpd_search_add_sort_tag( conn, MPD_TAG_TITLE, false );
