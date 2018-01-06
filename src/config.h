ec9aedd7a0e331d3c4fcda0d5a46f57c167bbce5
Alexander Wittig <alexander@wittig.name>
2018-01-06 22:36:47 +0000

    small cleanup

--- src/config.h
+++ src/config.h
@@ -1,5 +1,4 @@
 #define SOCKET_PFAD             "/run/mpd/socket"           // MPD socket
-#define OUTPUT_BUFFER_SIZE      (2*1024*1024)               // output buffer size (3 MB for now)
 #define REBOOT_WAIT             100000                      // time in micro-seconds to wait after sync()ing
 //#define SYSTEMD                                             // Use systemd to reboot/shutdown
 //#define AUTOPLAY                                            // Automatically start playing when loading playlists
