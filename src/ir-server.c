15ab4181c1dd506375f95c1aaea2e0e6e9480285
Alexander Wittig <alexander@wittig.name>
2018-01-27 15:06:08 +0000

    fix

--- src/ir-server.c
+++ src/ir-server.c
@@ -692,8 +692,9 @@ static int handle_request( req *c )
             for( unsigned int i = 0; (rc == FILE_NOT_FOUND) && conf.contents[i].url; i++ )
             {
                 // check Host
+                debug_printf( "===> Hosts: \"%s\"\n", c->host ? c->host : "" );
                 if( conf.contents[i].host && c->host && !strcmp( conf.contents[i].host, c->host ) ) continue;
-                debug_printf( "===> Hosts: \"%s\" \"%s\"\n", conf.contents[i].host ? conf.contents[i].host : "", c->host ? c->host : "" );
+                debug_printf( "     matches: \"%s\"\n", conf.contents[i].host ? conf.contents[i].host : "" );
 
                 // check URL
                 if( conf.contents[i].flags & CONT_PREFIX_MATCH )
